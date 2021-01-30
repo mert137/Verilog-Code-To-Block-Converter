16970581eea381d2a9a0c4745164aa28a339e620
Mert Salar <mertsalar137@gmail.com>
2021-01-30 16:56:08 +0300

    Update canvas.py

--- canvas.py
+++ canvas.py
@@ -5,14 +5,11 @@ from PyQt5.QtGui import QPolygon
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtCore import Qt
 
+
 class Canvas(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setGeometry(30, 30, 600, 400)
-
-        self.in_order = 0
-        self.out_order = 0
-        self.inout_order = 0
 
         self.show()
         self.rect_list = []
@@ -31,33 +28,42 @@ class Canvas(QtWidgets.QWidget):
             qp.drawText((r.rect_begin + r.rect_end) / 2, r.center_text)  # Write the name of module rectangle
 
             # Draw input ports and their names
+            in_order = 0
+            out_order = 0
+            inout_order = 0
             for i in r.port_list:
                 if i.port_type == 'input':
-                    i.points = [r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H), int(r.Tri_In_F * i.in_order)),
-                                r.rect_begin + QtCore.QPoint(0, int(r.Tri_In_F / 2 + r.Tri_In_F * i.in_order)),
+                    i.points = [r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H), int(r.Tri_In_F * in_order)),
+                                r.rect_begin + QtCore.QPoint(0, int(r.Tri_In_F / 2 + r.Tri_In_F * in_order)),
                                 r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H),
-                                                             int(r.Tri_In_F + r.Tri_In_F * i.in_order))]
-                    qp.drawText(r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H), int(r.Tri_In_F * i.in_order)),
-                                i.text)  # Write the port name
+                                                             int(r.Tri_In_F + r.Tri_In_F * in_order))]
+
+                    # Write the port name
+                    qp.drawText(r.rect_begin + QtCore.QPoint(5, int(r.Tri_In_F / 2 + r.Tri_In_F * in_order)), i.text)
+                    in_order = in_order + 1
+
                 elif i.port_type == 'output':
-                    i.points = [r.rect_end + QtCore.QPoint(int(r.Tri_In_H),
-                                                           int(-r.Tri_In_F / 2 - r.Tri_In_F * i.out_order)),
-                                r.rect_end + QtCore.QPoint(0, int(-r.Tri_In_F - r.Tri_In_F * i.out_order)),
-                                r.rect_end + QtCore.QPoint(0, int(- r.Tri_In_F * i.out_order))]
-                    qp.drawText(r.rect_end + QtCore.QPoint(int(r.Tri_In_H),
-                                                           int(-r.Tri_In_F / 2 - r.Tri_In_F * i.out_order)),
-                                i.text)  # Write the port name
+                    i.points = [r.rect_end + QtCore.QPoint(int(r.Tri_In_H + 1),
+                                                           int(-r.Tri_In_F / 2 - r.Tri_In_F * out_order)),
+                                r.rect_end + QtCore.QPoint(1, int(-r.Tri_In_F - r.Tri_In_F * out_order)),
+                                r.rect_end + QtCore.QPoint(1, int(- r.Tri_In_F * out_order))]
+
+                    # Write the port name
+                    qp.drawText(r.rect_end + QtCore.QPoint(int(r.Tri_In_H + 5), int(-r.Tri_In_F / 2 - r.Tri_In_F * out_order)), i.text)
+                    out_order = out_order + 1
+
                 elif i.port_type == 'inout':
-                    i.points = [r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H), int(r.Tri_In_F * i.inout_order)),
-                                r.rect_begin + QtCore.QPoint(0, int(r.Tri_In_F / 2 + r.Tri_In_F * i.inout_order)),
-                                r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H),
-                                                             int(r.Tri_In_F + r.Tri_In_F * i.inout_order)),
-                                r.rect_begin + QtCore.QPoint(int(-2 * r.Tri_In_H),
-                                                             int(r.Tri_In_F / 2 + r.Tri_In_F * i.inout_order))]
-                    qp.drawText(r.rect_begin + QtCore.QPoint(int(-r.Tri_In_H), int(r.Tri_In_F * i.inout_order)),
-                                i.text)  # Write the port name
-                i.set_polygon()
-                qp.drawPolygon(i.polygon)
+                    i.points = [QtCore.QPoint(int(r.rect_begin.x() - r.Tri_In_H), int(r.rect_end.y() - r.Tri_In_F - r.Tri_In_F * inout_order)),
+                                QtCore.QPoint(int(r.rect_begin.x()), int(r.rect_end.y() - r.Tri_In_F / 2 - r.Tri_In_F * inout_order)),
+                                QtCore.QPoint(int(r.rect_begin.x() - r.Tri_In_H), int(r.rect_end.y() - r.Tri_In_F * inout_order)),
+                                QtCore.QPoint(int(r.rect_begin.x() - 2 * r.Tri_In_H), int(r.rect_end.y() - r.Tri_In_F / 2 - r.Tri_In_F * inout_order))]
+
+                    # Write the port name
+                    qp.drawText(QtCore.QPoint(int(r.rect_begin.x() + 5), int(r.rect_end.y() - r.Tri_In_F / 2 - r.Tri_In_F * inout_order)), i.text)
+                    inout_order = inout_order + 1
+
+                polygon = QPolygon(i.points)
+                qp.drawPolygon(polygon)
 
     # When mouse is pressed, the top left corner of the rectangle being drawn is saved
     def mousePressEvent(self, event):
@@ -85,72 +91,42 @@ class Canvas(QtWidgets.QWidget):
                 self.update()
 
     def contextMenuEvent(self, event):
-        contextMenu = QMenu(self)
+        for i in self.rect_list:
+            if i.rect_begin.x() < event.pos().x() < i.rect_end.x() and i.rect_begin.y() < event.pos().y() < i.rect_end.y():
+                contextMenu = QMenu(self)
 
-        # Add Input Port action is used to add input port to both code and block
-        inputAction = contextMenu.addAction("Add Input Port")
-        # Add Output Port action is used to add output port to both code and block
-        outputAction = contextMenu.addAction("Add Output Port")
-        # Add Inout Port action is used to add inout port to both code and block
-        inoutAction = contextMenu.addAction("Add Inout Port")
+                # Add Input Port action is used to add input port to both code and block
+                inputAction = contextMenu.addAction("Add Input Port")
+                # Add Output Port action is used to add output port to both code and block
+                outputAction = contextMenu.addAction("Add Output Port")
+                # Add Inout Port action is used to add inout port to both code and block
+                inoutAction = contextMenu.addAction("Add Inout Port")
 
-        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+                action = contextMenu.exec_(self.mapToGlobal(event.pos()))
 
-        if action == inputAction:
-            self.draw_input(self.rect_list[-1], "input_port_1")
-        elif action == outputAction:
-            self.draw_output(self.rect_list[-1], "output_port_1")
-        elif action == inoutAction:
-            self.draw_inout(self.rect_list[-1], "inout_port_1")
+                if action == inputAction:
+                    self.add_input(i, "input_port_1")
+                elif action == outputAction:
+                    self.add_output(i, "output_port_1")
+                elif action == inoutAction:
+                    self.add_inout(i, "inout_port_1")
 
-    def draw_input(self, module, text):
+    def add_input(self, module, text):
         tempClass = Port()
         tempClass.port_type = 'input'
-        tempClass.points = \
-            [
-                module.rect_begin + QtCore.QPoint(int(-module.Tri_In_H), int(module.Tri_In_F * tempClass.in_order)),
-                module.rect_begin + QtCore.QPoint(0, int(module.Tri_In_F / 2 + module.Tri_In_F * tempClass.in_order)),
-                module.rect_begin + QtCore.QPoint(int(-module.Tri_In_H),
-                                                  int(module.Tri_In_F + module.Tri_In_F * tempClass.in_order))
-            ]
-        tempClass.set_polygon()
         tempClass.text = text
-        tempClass.in_order = self.in_order
-        self.in_order = self.in_order + 1
         module.port_list.append(tempClass)
 
-    def draw_output(self, module, text):
+    def add_output(self, module, text):
         tempClass = Port()
         tempClass.port_type = 'output'
-        tempClass.points = \
-            [
-                module.rect_end + QtCore.QPoint(int(module.Tri_In_H),
-                                                int(-module.Tri_In_F / 2 - module.Tri_In_F * tempClass.out_order)),
-                module.rect_end + QtCore.QPoint(0, int(-module.Tri_In_F - module.Tri_In_F * tempClass.out_order)),
-                module.rect_end + QtCore.QPoint(0, int(- module.Tri_In_F * tempClass.out_order))
-            ]
-        tempClass.set_polygon()
         tempClass.text = text
-        tempClass.out_order = self.out_order
-        self.out_order = self.out_order + 1
         module.port_list.append(tempClass)
 
-    def draw_inout(self, module, text):
+    def add_inout(self, module, text):
         tempClass = Port()
         tempClass.port_type = 'inout'
-        tempClass.points = \
-            [
-                module.rect_begin + QtCore.QPoint(int(-module.Tri_In_H), int(module.Tri_In_F * tempClass.in_order)),
-                module.rect_begin + QtCore.QPoint(0, int(module.Tri_In_F / 2 + module.Tri_In_F * tempClass.in_order)),
-                module.rect_begin + QtCore.QPoint(int(-module.Tri_In_H),
-                                                  int(module.Tri_In_F + module.Tri_In_F * tempClass.in_order)),
-                module.rect_begin + QtCore.QPoint(int(-2 * module.Tri_In_H),
-                                                  int(module.Tri_In_F / 2 + module.Tri_In_F * tempClass.in_order))
-            ]
-        tempClass.set_polygon()
         tempClass.text = text
-        tempClass.inout_order =  self.in_order   # self.inout_order
-        self.in_order = self.in_order + 1        # self.inout_order = self.inout_order + 1
         module.port_list.append(tempClass)
 
 
@@ -159,22 +135,15 @@ class Port:
         self.points = [QtCore.QPoint()]
         self.polygon = QPolygon(self.points)
         self.text = ''
-        self.in_order = 0
-        self.out_order = 0
-        self.inout_order = 0
-        self.set_polygon()
         self.port_type = 'empty'
-
-    def set_polygon(self):
-        self.polygon = QPolygon(self.points)
 
 
 class Module:
     def __init__(self):
-        self.rect_begin = QtCore.QPoint()  # Module rectangle top left corner
-        self.rect_end = QtCore.QPoint()  # Module rectangle bottom right corner
+        self.rect_begin = QtCore.QPoint()   # Module rectangle top left corner
+        self.rect_end = QtCore.QPoint()     # Module rectangle bottom right corner
         self.Tri_In_H = 20
         self.Tri_In_F = 2 * self.Tri_In_H
-        self.center_text = ''  # Module name in the center of rectangle
+        self.center_text = ''               # Module name in the center of rectangle
         self.first_drawn = 0
         self.port_list = []
